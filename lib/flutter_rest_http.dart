8ac257e64d7d42d7779b70c7ddec9794a988ee5e
Yamikani Kalinde <yamikanikalinde@gmail.com>
2023-08-05 13:43:47 +0200

    add get post put and delete methods

--- lib/flutter_rest_http.dart
+++ lib/flutter_rest_http.dart
@@ -1,7 +1,200 @@
 library flutter_rest_http;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+typedef GlobalHeadersCallback = Map<String, String> Function();
+
+String _baseURL = "https://example.com";
+String _defaultError = "Cannot complete your request. Please try again";
+String _connectionError = "Please check your internet connection";
+String _timeoutError = "Your request timed out. Please try again";
+
+GlobalHeadersCallback? _globalHeaders;
+
+Uri getUrl(String path, {bool withBase = true}) {
+  var link = withBase ? "$_baseURL$path" : path;
+  if (kDebugMode) {
+    print(link);
+  }
+  return Uri.parse(link);
+}
+
+class RestHttp {
+  static void init({String? baseURL, GlobalHeadersCallback? globalHeaders}) {
+    if (baseURL != null) {
+      _baseURL = baseURL;
+    }
+    _globalHeaders = globalHeaders;
+  }
+
+  static Future<ApiRawResponse> _processRawResponse(
+      Future<http.Response> request) async {
+    try {
+      var response = await request.timeout(const Duration(seconds: 30));
+      if (kDebugMode) {
+        print(response.body);
+      }
+      return ApiRawResponse(response.statusCode, response.body);
+    } on TimeoutException {
+      throw ApiException(title: "Network Error", message: _timeoutError);
+    } on SocketException {
+      throw ApiException(title: "Network Error", message: _connectionError);
+    } catch (err, stack) {
+      if (kDebugMode) {
+        print(stack);
+      }
+      if (err is ApiException) {
+        throw ApiException(title: err.title, message: err.message);
+      }
+      throw ApiException(title: "Request Error", message: _defaultError);
+    }
+  }
+
+  static Future<ApiResponse> _processResponse(
+      Future<http.Response> request) async {
+    try {
+      var response = await request.timeout(const Duration(seconds: 5));
+      if (kDebugMode) {
+        print(response.statusCode);
+        print(response.body);
+      }
+
+      return ApiResponse(
+          response.statusCode,
+          response.statusCode.mayHaveBody()
+              ? json.decode(response.body)
+              : null);
+    } on TimeoutException {
+      throw ApiException(title: "Network Error", message: _timeoutError);
+    } on SocketException {
+      throw ApiException(title: "Network Error", message: _connectionError);
+    } catch (err, stack) {
+      if (kDebugMode) {
+        print(err);
+        print(stack);
+      }
+
+      if (err is ApiException) {
+        throw ApiException(title: err.title, message: err.message);
+      }
+      throw ApiException(title: "Request Error", message: _defaultError);
+    }
+  }
+
+  static Future<Map<String, String>> _getHeaders(
+      Map<String, String>? headers) async {
+    headers ??= <String, String>{};
+    headers.addAll({
+      "Accept": "application/json",
+      "Content-Type": "application/json; charset=UTF-8"
+    });
+
+    if (_globalHeaders != null) {
+      headers.addAll(_globalHeaders!());
+    }
+
+    if (kDebugMode) {
+      print("Headers: $headers");
+    }
+    return headers;
+  }
+
+  static Future<ApiResponse> get(String path,
+      {Map<String, String>? headers}) async {
+    return _processResponse(
+        http.get(getUrl(path), headers: await _getHeaders(headers)));
+  }
+
+  static Future<ApiRawResponse> getRaw(String path,
+      {Map<String, String>? headers}) async {
+    return _processRawResponse(http.get(getUrl(path, withBase: false),
+        headers: await _getHeaders(headers)));
+  }
+
+  static Future<ApiResponse> post(String path,
+      {Map<String, dynamic>? params, Map<String, String>? headers}) async {
+    var json = jsonEncode(params);
+
+    if (kDebugMode) {
+      print("JSON");
+      print(json);
+    }
+
+    return _processResponse(http.post(getUrl(path),
+        body: jsonEncode(params), headers: await _getHeaders(headers)));
+  }
+
+  static Future<ApiResponse> patch(String path,
+      {Map<String, String?>? params, Map<String, String>? headers}) async {
+    return _processResponse(http.patch(getUrl(path),
+        body: params, headers: await _getHeaders(headers)));
+  }
+
+  static List<ValidationError> getValidationErrors(dynamic response) {
+    try {
+      return response.body["errors"].keys.map<ValidationError>((k) {
+        return ValidationError(
+            key: k, errors: List<String>.from(response.body["errors"][k]));
+      }).toList();
+    } catch (err) {
+      if (kDebugMode) {
+        print(err);
+      }
+      return [];
+    }
+  }
+}
+
+class ApiResponse {
+  int code;
+  dynamic body;
+
+  ApiResponse(this.code, this.body);
+}
+
+class ApiRawResponse {
+  int code;
+  String body;
+
+  ApiRawResponse(this.code, this.body);
+}
+
+class ApiException implements Exception {
+  final String? title;
+  final String? message;
+
+  ApiException({this.title, this.message});
+}
+
+class ValidationError {
+  String? key;
+  List<String>? errors;
+
+  ValidationError({this.key, this.errors});
+}
+
+extension HttpStatusCodes on int {
+  bool httpSuccess() {
+    return this >= 200 && this < 300;
+  }
+
+  bool mayHaveBody() {
+    return httpSuccess() ||
+        httpBadRequest() ||
+        httpUnauthenticated() ||
+        this == 412;
+  }
+
+  bool httpBadRequest() {
+    return this == 400;
+  }
+
+  bool httpUnauthenticated() {
+    return this == 401;
+  }
 }
--- test/flutter_rest_http_test.dart
+++ test/flutter_rest_http_test.dart
@@ -4,9 +4,18 @@ import 'package:flutter_rest_http/flutter_rest_http.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    RestHttp.init(
+      baseURL: "https://google.com/",
+      globalHeaders: () {
+        return {"Authorization": "Bearer 123"};
+      },
+    );
+
+    RestHttp.get("hello");
+    expect(true, 2 > 1);
+
+    // expect(calculator.addOne(2), 3);
+    // expect(calculator.addOne(-7), -6);
+    // expect(calculator.addOne(0), 1);
   });
 }
